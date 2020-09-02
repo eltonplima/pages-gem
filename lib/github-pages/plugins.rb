51ba97f820a590de87cbe343c560bc163342e5c1
Elton Lima <eltonplima@gmail.com>
2020-09-02 21:55:24 +0100

    Update plugins.rb

--- lib/github-pages/plugins.rb
+++ lib/github-pages/plugins.rb
@@ -49,7 +49,7 @@ module GitHubPages
     # Themes
     THEMES = {
       "jekyll-swiss" => "1.0.0",
-      "minima" => "3.0.0",
+      "minima" => "2.5.0",
       "jekyll-theme-primer" => "0.5.4",
       "jekyll-theme-architect" => "0.1.1",
       "jekyll-theme-cayman" => "0.1.1",
